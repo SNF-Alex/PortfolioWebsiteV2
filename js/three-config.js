faf048ebab22f86a78b999f20c268cbe647c4388
Alex pace <aep2cool@gmail.com>
2025-05-05 21:55:20 -0400

    Fix Partical Background?

--- js/three-config.js
+++ js/three-config.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     scene = new THREE.Scene();
     
-    camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 1000);
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
     camera.position.z = 2;
 
     renderer = new THREE.WebGLRenderer({
